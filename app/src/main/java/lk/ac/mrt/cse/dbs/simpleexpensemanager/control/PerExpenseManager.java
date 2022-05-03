9a3540994d8da66c97f4d046788ba97db3d3872d
pasidu <80567526+PasiduSupunsara@users.noreply.github.com>
2022-05-03 21:18:06 +0530

    Update PerExpenseManager.java

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PerExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PerExpenseManager.java
@@ -5,8 +5,8 @@ import android.content.Context;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
-import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistantAccountDAO;
-import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentTransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PerAccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PerTransactionDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
 
 public class PerExpenseManager extends ExpenseManager{
